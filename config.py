98f91f975a63c4af2dd3f93c05737da5b1e7e0ab
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-04-06 18:01:31 -0400

    I included the Echo Time and the Noise level

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import numpy as np
 
 # True model parameters for data generation
-TRUE_PARAMS = {"C1": 0.4}
+TRUE_PARAMS = {"C1": 0.4, "C2": 0.6, "T21": 30, "T22": 200}
+
+# Echo Times
+TE = np.linspace(0, 500, 1000)
+
+# Noise levels
+σ = [0.01, 0.05, 0.1, 0.2]
