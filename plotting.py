ec4b1d5b56f678256b8030de9655874c2161e1b8
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-04-06 22:59:35 -0400

    Completed the plotting

---
+++ plotting.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def plot_signals(
+    TE, clean_signal, noisy_signal=None, title="Generating Bi-exponential Data"
+):
+    """
+    Plot clean and noisy signal
+
+    Parameters
+    ----------
+    TE: 1D array
+    clean_signal: 1D array
+    noisy_signal: 1D array
+    title: str
+    """
+
+    # Seaborn config
+    sns.set_theme(style="whitegrid", context="talk")
+    palette = sns.color_palette("deep")
+
+    plt.figure(figsize=(9, 5), dpi=100)
+    plt.plot(TE, clean_signal, linewidth=2.5, color=palette[0], label="Clean signal")
+
+    if noisy_signal is not None:
+        plt.scatter(
+            TE,
+            noisy_signal,
+            s=30,
+            color=palette[1],
+            alpha=0.7,
+            edgecolors="black",
+            linewidths=0.5,
+            label="Noisy data",
+        )
+
+    plt.xlabel("TE", fontsize=12)
+    plt.ylabel("S(TE)", fontsize=12)
+    plt.title(title, fontsize=14, weight="bold")
+    plt.legend(frameon=True)
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
