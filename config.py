778f59b4ec6b8220720f11562e3bf11d70776832
Adebayo Emmanuel <immaculatedcoder@gmail.com>
2026-04-11 23:19:35 -0400

    Updating list of files

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # True model parameters for data generation
-TRUE_PARAMS = {"C1": 0.4, "C2": 0.6, "T21": 30, "T22": 200}
+TRUE_PARAMS = {"C1": 0.5, "C2": 0.5, "T21": 30, "T22": 200}
 
 # Echo Times
 TE = np.linspace(0, 500, 1000)
